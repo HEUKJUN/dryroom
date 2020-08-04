9ac3a5050842a39d7abf1c271f694f783c6ed35e
Woobin Lee <ggompang0206@gmail.com>
2020-08-04 10:53:38 +0900

    Graph Step 1

--- app.py
+++ app.py
@@ -3,12 +3,37 @@ from flask import Flask ,render_template, flash, redirect, url_for, session, req
 import pymysql
 from passlib.hash import pbkdf2_sha256
 from functools import wraps
+import json
+import paho.mqtt.client as mqtt
 
-app = Flask(__name__)
-app.debug=True
+temperature = []
 
 db = pymysql.connect(host='localhost', port=3306, user='root', passwd='1234', db='dryroom')
 
+app = Flask(__name__)
+app.debug=False
+
+def on_connect( client, userdata, flags, rc ):
+    # print("Connect with result code" + str(rc) )
+    client.subscribe('temp')
+
+def on_message( client, userdata, msg ):
+    # print(float(msg.payload))
+    # temp = []
+    temperature.append(float(msg.payload))
+    # cursor = db.cursor()
+    # sql =  '''
+    #      INSERT INTO arduino(humidity, temperature) 
+    #      VALUES (%s, %s);
+    #     '''
+    # cursor.execute(sql, (temp[0], temp[1]))
+    # db.commit()
+    # db.close()
+    print(float(msg.payload))
+    # print(temp[1])
+
+
+    
 def is_logged_in(f):
     @wraps(f)
     def _wraper(*args, **kwargs):
@@ -28,7 +53,7 @@ def login():
         id = request.form.get('username')
         pw = request.form.get('password')
         # print([id , pw])
-
+        
         sql='SELECT * FROM users WHERE username = %s'
         cursor  = db.cursor()
         cursor.execute(sql, [id])
@@ -246,13 +271,76 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-@app.route('/graph')
+@app.route('/graph', methods=['GET', 'POST'])
 @is_logged_in
 def graph():
+    cursor = db.cursor()
+    sql = 'SELECT humidity, register_date FROM arduino ORDER BY register_date DESC limit 10;'
+    cursor.execute(sql)
+    db.commit()
+    data_all = cursor.fetchall()
+    print(data_all)
+    data_temperature = []
+    data_register_date = []
+  
+    for i, j  in reversed(data_all):
+        data_temperature.append(i)
+        data_register_date.append(str(j.date()))
 
-    list_data =[0, 50, 5, 2, 20, 30, 4]
-    return render_template('graph.html', data = list_data)
+    print(data_register_date)
+
+    return render_template('graph.html', data1 = data_temperature, data2 = data_register_date)
+
+    # for i in data_all:
+    #     # print(i)
+    #     # data_temperature.append(int(i[1]))
+    #     print(len(data_temperature))
+    #     print(data_all)
+    #     if len(data_temperature) > 9:
+    #         del data_temperature[0]
+            
+    #         data_temperature.append(int(data_all[len(data_all)-1][1]))
+    #         # print(data_all[len(data_all)-1][1])
+    #         # print(len(data_temperature))
+    #         # data_temperature.append(int(i[1]))
+
+    #     else:
+    #         data_temperature.append(int(i[1]))
+        
+    #     # data_register_date.append(str(j.time()))
+    
+    # print(data_temperature)
+
+
+@app.route('/test')
+def test():
+    cursor = db.cursor()
+    sql = 'SELECT temperature, register_date FROM temperature;'
+    cursor.execute(sql)
+    data_all = cursor.fetchall()
+    data_temperature = []
+    data_register_date = []
+    for i, j  in data_all :
+        data_temperature.append(int(i))
+        data_register_date.append(str(j))
+
+    return render_template('test.html', data = [data_temperature, data_register_date])
+
+@app.route('/interval')
+def interval():
+    return render_template('interval.html')
+
+@app.route('/arduino', methods = ['GET'])
+def hello_world():
+    return str(temperature)
+    # return render_template('interval.html', data =str(temperature) )  
 
 if __name__ =='__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("localhost", 1883, 60)
+    client.loop_start()
+
     app.secret_key = 'secretkey123456789'
     app.run(host='0.0.0.0', port='8000')
